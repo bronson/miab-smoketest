9bc867e6b4abcd6565735c52580f3a43656767bb
Scott Bronson <brons_git@rinspin.com>
2015-11-28 16:52:44 -0800

    this redirect doesn't include a link in the body

--- spec/http_spec.rb
+++ spec/http_spec.rb
@@ -17,8 +17,8 @@ describe 'http' do
   def ensure_redirect response, url_matcher
     expect(response.code).to eq('301').or eq('302')
     expect(response.header['location']).to match url_matcher
-    href = document(response.body).css('a').attr('href').value
-    expect(href).to match url_matcher
+    # href = document(response.body).css('a').attr('href').value
+    # expect(href).to match url_matcher
     response.header['location']
   end
 
