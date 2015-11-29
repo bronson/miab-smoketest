0ac124ce42e3d623bbfb134db2cfdcd1af53d84f
Scott Bronson <brons_git@rinspin.com>
2015-11-28 17:18:15 -0800

    allow specifying HOST via environment variable

--- config.rb
+++ config.rb
@@ -1,4 +1,4 @@
-HOST='127.0.0.1'          # the host to test, either IP address or domain name
+HOST=ENV['HOST'] || '127.0.0.1'  # the host to test, either IP address or domain name
 
 # The following is only used to test the correctness of responses
 BOX_NAME="int"            # the hostname of the server running miab
